fae2b05a2be39989f1902926ed967608f912f96e
vsabolcec <vinko.sabolcec1@gmail.com>
2024-02-14 07:37:17 +0100

    Update examples and lang_stats.json

--- examples/multilingual/extract_lang_stats.py
+++ examples/multilingual/extract_lang_stats.py
@@ -9,78 +9,80 @@ from datatrove.pipeline.stats import LanguageStats, LanguageStatsReducer
 
 # Top 10 languages other than English in CommonCrawl according to (https://arxiv.org/pdf/2306.01116.pdf)
 # TODO: ja, zh don't have good tokenizers
-# LANGUAGES = ["ru", "de", "es", "ja", "fr", "zh", "it", "pt", "nl", "pl"]
-LANGUAGES = ["fr", "de"]
+LANGUAGES = ["ru", "de", "es", "ja", "fr", "zh", "it", "pt", "nl", "pl"]
 MAIN_OUTPUT_PATH = "./language_stats"
 WIKI_VERSION = "20231101"  # See https://huggingface.co/datasets/wikimedia/wikipedia
-DOC_LIMIT = 20000  # Limit number of documents per dataset
+DOC_LIMIT = 50000  # Limit number of documents per dataset
 EXECUTOR = os.environ.get("EXECUTOR", "slurm")  # local/slurm
 
-# Count token lengths
-readers = [
-    HuggingFaceDatasetReader(
-        "wikimedia/wikipedia",
-        limit=DOC_LIMIT,
-        dataset_options={
-            "name": f"{WIKI_VERSION}.{language}",
-            "split": "train",
-        },
-    )
-    for language in LANGUAGES
-]
+if __name__ == "__main__":
+    # Count token lengths
+    readers = [
+        HuggingFaceDatasetReader(
+            "wikimedia/wikipedia",
+            limit=DOC_LIMIT,
+            dataset_options={
+                "name": f"{WIKI_VERSION}.{language}",
+                "split": "train",
+            },
+        )
+        for language in LANGUAGES
+    ]
 
-pipeline = [
-    *readers,
-    LanguageFilter(languages=(LANGUAGES)),
-    LanguageStats(output_folder=f"{MAIN_OUTPUT_PATH}/lang_stats/"),
-]
-executor = {
-    "local": LocalPipelineExecutor(pipeline=pipeline, logging_dir=f"{MAIN_OUTPUT_PATH}/logs/"),
-    "slurm": SlurmPipelineExecutor(
-        pipeline=pipeline,
-        logging_dir=f"{MAIN_OUTPUT_PATH}/logs/",
-        tasks=2,
-        time="00:30:00",
-        partition="clariden",
-    ),
-}[EXECUTOR]
-executor.run()
-
-
-# Reduce token lengths into lang_stats.json file
-def length_counter_reducer(length_counter):
-    # Make sure to import np here for slurm executor
-    import numpy as np
-
-    lengths = list(length_counter.keys())
-    freqs = list(length_counter.values())
-
-    word_length_mean = np.average(lengths, weights=freqs)
-    word_length_std = np.sqrt(np.cov(lengths, fweights=freqs))
-
-    return {
-        "min_avg_word_length": round(word_length_mean - word_length_std),
-        "max_avg_word_length": round(word_length_mean + word_length_std),
-    }
+    pipeline = [
+        *readers,
+        LanguageFilter(languages=(LANGUAGES)),
+        LanguageStats(output_folder=f"{MAIN_OUTPUT_PATH}/lang_stats/"),
+    ]
+    executor = {
+        "local": LocalPipelineExecutor(pipeline=pipeline, logging_dir=f"{MAIN_OUTPUT_PATH}/logs/", tasks=8),
+        "slurm": SlurmPipelineExecutor(
+            pipeline=pipeline,
+            logging_dir=f"{MAIN_OUTPUT_PATH}/logs/",
+            tasks=2,
+            time="00:30:00",
+            partition="clariden",
+        ),
+    }[EXECUTOR]
+    executor.run()
 
 
-pipeline_reduce = [
-    LanguageStatsReducer(
-        input_folder=f"{MAIN_OUTPUT_PATH}/lang_stats/",
-        output_folder=".",
-        output_file_name="lang_stats.json",
-        length_counter_reducer=length_counter_reducer,
-    )
-]
-executor_reduce = {
-    "local": LocalPipelineExecutor(pipeline=pipeline_reduce, logging_dir=f"{MAIN_OUTPUT_PATH}/logs_reduce/"),
-    "slurm": SlurmPipelineExecutor(
-        pipeline=pipeline_reduce,
-        logging_dir=f"{MAIN_OUTPUT_PATH}/logs_reduce/",
-        tasks=1,
-        time="00:30:00",
-        partition="clariden",
-        depends=executor,
-    ),
-}[EXECUTOR]
-executor_reduce.run()
+    # Reduce token lengths into lang_stats.json file
+    def length_counter_reducer(length_counter):
+        # Make sure to import np here for slurm executor
+        import numpy as np
+
+        lengths = list(length_counter.keys())
+        freqs = list(length_counter.values())
+
+        word_length_mean = np.average(lengths, weights=freqs)
+        word_length_std = np.sqrt(np.cov(lengths, fweights=freqs))
+
+        return {
+            "min_avg_word_length": round(word_length_mean - word_length_std),
+            "max_avg_word_length": round(word_length_mean + word_length_std),
+            'word_length_mean': word_length_mean,
+            'word_length_std': word_length_std,
+        }
+
+
+    pipeline_reduce = [
+        LanguageStatsReducer(
+            input_folder=f"{MAIN_OUTPUT_PATH}/lang_stats/",
+            output_folder=".",
+            output_file_name="lang_stats.json",
+            length_counter_reducer=length_counter_reducer,
+        )
+    ]
+    executor_reduce = {
+        "local": LocalPipelineExecutor(pipeline=pipeline_reduce, logging_dir=f"{MAIN_OUTPUT_PATH}/logs_reduce/"),
+        "slurm": SlurmPipelineExecutor(
+            pipeline=pipeline_reduce,
+            logging_dir=f"{MAIN_OUTPUT_PATH}/logs_reduce/",
+            tasks=1,
+            time="00:30:00",
+            partition="clariden",
+            depends=executor,
+        ),
+    }[EXECUTOR]
+    executor_reduce.run()
--- examples/multilingual/process_fineweb.py
+++ examples/multilingual/process_fineweb.py
@@ -15,7 +15,7 @@ EXECUTOR = os.environ.get("EXECUTOR", "slurm")  # local/slurm
 DATASET = "HuggingFaceFW/fineweb_german_extract"
 MAIN_OUTPUT_PATH = "./fineweb_processed"
 STATS_FILE = "./lang_stats.json"
-DOC_LIMIT = 1000  # Limit number of documents per dataset
+DOC_LIMIT = 100000  # Limit number of documents per dataset
 
 # Load language-specific statistics and stopwords
 with open(STATS_FILE, "r") as f:
@@ -28,31 +28,33 @@ stopwords = {
     "fr": stopwords.words("french"),
 }
 
-# Process dataset
-pipeline = [
-    HuggingFaceDatasetReader(DATASET, dataset_options={"token": HF_TOKEN, "split": "train"}, limit=DOC_LIMIT),
-    GopherRepetitionFilter(),
-    MultilingualGopherQualityFilter(
-        max_avg_word_lengths=max_avg_word_lengths,
-        min_avg_word_lengths=min_avg_word_lengths,
-        stop_words=stopwords,
-    ),
-    ListFilter(),
-    JsonlWriter(f"{MAIN_OUTPUT_PATH}/output/{DATASET}"),
-]
+if __name__=="__main__":
+    # Process dataset
+    pipeline = [
+        HuggingFaceDatasetReader(DATASET, dataset_options={"token": HF_TOKEN, "split": "train"}, limit=DOC_LIMIT),
+        GopherRepetitionFilter(),
+        MultilingualGopherQualityFilter(
+            max_avg_word_lengths=max_avg_word_lengths,
+            min_avg_word_lengths=min_avg_word_lengths,
+            stop_words=stopwords,
+        ),
+        ListFilter(),
+        JsonlWriter(f"{MAIN_OUTPUT_PATH}/output/{DATASET}"),
+    ]
 
-executor = {
-    "slurm": SlurmPipelineExecutor(
-        pipeline=pipeline,
-        logging_dir=f"{MAIN_OUTPUT_PATH}/logs/{DATASET}",
-        tasks=2,
-        time="00:30:00",
-        partition="clariden",
-    ),
-    "local": LocalPipelineExecutor(
-        pipeline=pipeline,
-        logging_dir=f"{MAIN_OUTPUT_PATH}/logs/{DATASET}",
-    ),
-}[EXECUTOR]
+    executor = {
+        "slurm": SlurmPipelineExecutor(
+            pipeline=pipeline,
+            logging_dir=f"{MAIN_OUTPUT_PATH}/logs/{DATASET}",
+            tasks=2,
+            time="00:30:00",
+            partition="clariden",
+        ),
+        "local": LocalPipelineExecutor(
+            pipeline=pipeline,
+            logging_dir=f"{MAIN_OUTPUT_PATH}/logs/{DATASET}",
+            tasks=8,
+        ),
+    }[EXECUTOR]
 
-executor.run()
+    executor.run()
