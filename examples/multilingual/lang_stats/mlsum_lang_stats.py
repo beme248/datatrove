0c7e8d5af56025c853de24711465e75df09e61ef
vsabolcec <vinko.sabolcec1@gmail.com>
2024-02-26 10:26:28 +0100

    Remove util

--- examples/multilingual/lang_stats/mlsum_lang_stats.py
+++ examples/multilingual/lang_stats/mlsum_lang_stats.py
@@ -6,9 +6,6 @@ from datatrove.pipeline.filters import LanguageFilter
 from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.stats import LanguageStats, LanguageStatsReducer
 
-from util import stat_reducer
-
-
 # See https://huggingface.co/datasets/mlsum
 LANGUAGES = ["de", "es", "fr", "ru"]
 MAIN_OUTPUT_PATH = "./mlsum_language_stats"
@@ -46,6 +43,38 @@ if __name__ == "__main__":
         ),
     }[EXECUTOR]
     executor.run()
+
+    def stat_reducer(language_stats):
+        # Make sure to import np here for slurm executor
+        import numpy as np
+
+        def q(counts, q):
+            counts_sorted = sorted(counts)
+            xs = [d[0] for d in counts_sorted]
+            ys = [d[1] for d in counts_sorted]
+            ys_cumsum = np.cumsum(ys)
+            index = np.sum(ys_cumsum < q * ys_cumsum[-1])
+            return xs[index]
+
+        length_counter = language_stats["length_counter"]
+
+        lengths = list(length_counter.keys())
+        freqs = list(length_counter.values())
+
+        word_length_mean = np.average(lengths, weights=freqs)
+        word_length_std = np.sqrt(np.cov(lengths, fweights=freqs))
+        word_length_q = {f"{i/20:.2f}": q(length_counter.items(), i / 20) for i in range(21)}
+
+        top_8_words = dict(language_stats["word_counter"].most_common(8))
+
+        return {
+            "min_avg_word_length": round(word_length_mean - word_length_std),
+            "max_avg_word_length": round(word_length_mean + word_length_std),
+            "word_length_mean": word_length_mean,
+            "word_length_std": word_length_std,
+            "word_length_q": word_length_q,
+            "top_8_words": top_8_words,
+        }
 
     pipeline_reduce = [
         LanguageStatsReducer(
