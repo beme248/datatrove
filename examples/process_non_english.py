858585ba412b551be0a5265547a2debe3d2f1fdf
NXz64Fdf8Y <bmessmer@clariden-ln001>
2024-03-01 16:29:24 +0100

    adjust multi-lingual

---
+++ examples/process_non_english.py
@@ -0,0 +1,99 @@
+import sys
+
+from datatrove.executor.slurm import SlurmPipelineExecutor
+from datatrove.pipeline.filters import (
+    MultilingualGopherQualityFilter,
+    GopherRepetitionFilter,
+    ListFilter,
+)
+from datatrove.pipeline.readers import JsonlReader
+from datatrove.pipeline.writers.jsonl import JsonlWriter
+import os
+import json
+from nltk.corpus import stopwords
+from datatrove.utils.typeshelper import Languages
+
+
+# DUMP should be given as an argument. Example: CC-MAIN-2023-23
+if len(sys.argv) != 2:
+    print("Argument required: dump name")
+    sys.exit(-1)
+
+DUMP = sys.argv[1]
+DOC_LIMIT = 50000
+NUM_TASKS = 20
+STATS_FILE = "./examples/multilingual/lang_stats/wiki_lang_stats.json"
+# LANGUAGES = [Languages.russian, Languages.german, Languages.spanish, "ja", Languages.french, "zh", Languages.italian, Languages.polish, Languages.dutch, "pl"]
+LANGUAGES = [ Languages.russian, Languages.german, Languages.spanish, Languages.french, Languages.italian, Languages.portuguese, Languages.dutch, Languages.polish ]
+EN_LANGUAGES = {
+    Languages.english: "english",
+    Languages.russian: "russian",
+    Languages.german: "german",
+    Languages.spanish: "spanish",
+    Languages.french: "french",
+    Languages.italian: "italian",
+    Languages.portuguese: "portuguese",
+    Languages.dutch: "dutch"
+}
+
+STOP_WORDS = "stopwords_p_thresh_0_008"
+
+scratch = os.getenv('SCRATCH')
+
+# Load language-specific statistics and stopwords
+with open(STATS_FILE, "r") as f:
+    language_stats = json.load(f)
+
+def to_clean_stopwords(k, v):
+    if k not in EN_LANGUAGES:
+        return v["stopwords_p_thresh"]["0.008"]
+    clean_stopwords = stopwords.words(EN_LANGUAGES[k])
+    return [ word for word in v["stopwords_p_thresh"]["0.008"] if word in clean_stopwords ]
+
+min_avg_word_lengths = {k: v["min_avg_word_length"] for k, v in language_stats.items()}
+max_avg_word_lengths = {k: v["max_avg_word_length"] for k, v in language_stats.items()}
+stop_words = {k: to_clean_stopwords(k, v) for k, v in language_stats.items()}
+min_stop_words = {k: 2 for k, _ in language_stats.items()}
+
+print(f"======> {stop_words}")
+
+breakpoint()
+
+for lang in LANGUAGES:
+    additional_data_path = f'data/datatrove/multi_lingual_{DOC_LIMIT}_{STOP_WORDS}/base_processing/{lang}'
+    additional_logs_path = f'logs/datatrove/multi_lingual_{DOC_LIMIT}_{STOP_WORDS}/base_processing/{lang}'
+    data_path = f"s3://fineweb-data-processing-us-east-1/base_processing/non_english/{lang}/{DUMP}"
+
+    MAIN_OUTPUT_PATH = os.path.join(scratch, additional_data_path)
+    SLURM_LOGS = os.path.join(scratch, additional_logs_path)
+
+    executor = SlurmPipelineExecutor(
+        job_name=f"cc_{DUMP}",
+        pipeline=[
+            JsonlReader(
+                data_path,
+                default_metadata={"dump": DUMP},
+                limit=DOC_LIMIT,
+                text_key="content",
+            ),
+            GopherRepetitionFilter(exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/repetitive/{DUMP}")),
+            MultilingualGopherQualityFilter(
+                max_avg_word_lengths=max_avg_word_lengths,
+                min_avg_word_lengths=min_avg_word_lengths,
+                stop_words=stop_words,
+                min_stop_words=min_stop_words,
+                exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/quality/{DUMP}"),
+            ),
+            ListFilter(exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/list/{DUMP}")),
+            JsonlWriter(f"{MAIN_OUTPUT_PATH}/output/{DUMP}"),
+        ],
+        tasks=NUM_TASKS,
+        time="00:30:00",
+        logging_dir=f"{MAIN_OUTPUT_PATH}/logs/base_processing/{DUMP}",
+        slurm_logs_folder=f"{SLURM_LOGS}/{DUMP}/slurm_logs",
+        randomize_start=True,
+        mem_per_cpu_gb=2,
+        partition="normal",
+    )
+    executor.run()
+
---
+++ scripts/aggregate_stats.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+base_path = "/iopsstor/scratch/cscs/bmessmer/data/datatrove/multi_lingual_50000_stopwords_p_thresh_0_008/base_processing"
+languages = ["ru", "de", "es", "fr", "it", "pt", "nl", "pl"]
+DUMP = "CC-MAIN-2023-50"
+
+# Iterate through each language and process the JSON file
+print(f"================> updated {base_path}")
+for lang in languages:
+    file_path = os.path.join(base_path, lang, "logs", "base_processing", DUMP, "stats.json")
+    
+    try:
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+            
+            # Accessing the third item in the list under the top level
+            if len(data) > 2:  # Ensure there's at least 3 items
+                stats_data = data[2].get("stats", {})
+                total_num_docs = data[0].get("stats", {}).get("documents", {}).get("total", {})
+                
+                # Extract the required fields from the "stats" dictionary
+                dropped_gopher_above = stats_data.get("dropped_gopher_above_avg_threshold", {}).get("total", 0)
+                dropped_gopher_below = stats_data.get("dropped_gopher_below_avg_threshold", {}).get("total", 0)
+                stop_words_gopher = stats_data.get("dropped_gopher_enough_stop_words", {}).get("total", 0)
+                stats_total = stats_data.get("total", {}).get("total", 0)
+                dropped_total = stats_data.get("dropped", {}).get("total", 0)
+                
+                # Calculating ratios
+                dropped_gopher_above_ratio = dropped_gopher_above / dropped_total if dropped_total else 0
+                dropped_gopher_below_ratio = dropped_gopher_below / dropped_total if dropped_total else 0
+                stop_words_gopher_ratio = stop_words_gopher / dropped_total if dropped_total else 0
+                dropped_total_ratio = dropped_total / stats_total if stats_total else 0
+
+                filter_stats_data = data[3].get("stats", {})
+                list_filter = filter_stats_data.get("dropped_Suspected list", {}).get("total", 0)
+                filter_processed_total = filter_stats_data.get("total", {}).get("total", 0)
+                list_filter_ratio = list_filter / filter_processed_total if filter_processed_total else 0
+                
+                print(f"Language: {lang}")
+                print(f"Dropped Gopher Above Avg Threshold: {dropped_gopher_above} ({dropped_gopher_above_ratio:.2%} of dropped total)")
+                print(f"Dropped Gopher Below Avg Threshold: {dropped_gopher_below} ({dropped_gopher_below_ratio:.2%} of dropped total)")
+                print(f"Dropped Stopwords Threshold: {stop_words_gopher} ({stop_words_gopher_ratio:.2%} of dropped total)")
+                print(f"Dropped List Threshold: {list_filter} ({list_filter_ratio:.2%} of dropped total)")
+                print(f"Total Processed: {total_num_docs}")
+                print(f"Total Processed (Quality filter): {stats_total}")
+                print(f"Dropped Total: {dropped_total} ({dropped_total_ratio:.2%} of stats total)")
+                print("----------")
+            else:
+                print(f"Data for language {lang} does not contain enough items to process.")
+                
+    except FileNotFoundError:
+        print(f"File not found for language {lang}: {file_path}")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from file for language {lang}: {file_path}")
