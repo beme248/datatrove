47152beaf3b143dd6b2f3629e90df45f38c0512d
vsabolcec <vinko.sabolcec1@gmail.com>
2024-03-14 16:36:22 +0100

    Format code

--- tests/pipeline/test_word_tokenizers.py
+++ tests/pipeline/test_word_tokenizers.py
@@ -1,11 +1,13 @@
 import unittest
+
 from datatrove.tools.word_tokenizers import WORD_TOKENIZERS
 
-sample_text = "Hello world! ქართული " * 500
+
+sample_text = "Hello world! ქართული " * 100
+
 
 class TestWordTokenizers(unittest.TestCase):
     def test_word_tokenizers(self):
         for language in WORD_TOKENIZERS:
             tokenizer = WORD_TOKENIZERS[language]
             assert len(tokenizer.tokenize(sample_text)) >= 1, f"'{language}' tokenizer assertion failed"
-
