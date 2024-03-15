705e4120b29ec00c342df6c71aedc0d757cece52
NXz64Fdf8Y <bmessmer@clariden-ln001>
2024-03-15 11:56:08 +0100

    draft 6

--- examples/process_non_english.py
+++ examples/process_non_english.py
@@ -25,7 +25,7 @@ NUM_TASKS = 20
 STATS_FILE = "./examples/multilingual/lang_stats/wiki_lang_stats_100.json"
 TOP50_LANGUAGES = [ Languages.english, Languages.spanish, Languages.portuguese, Languages.italian, Languages.french, Languages.romanian, Languages.german, Languages.latin, Languages.czech, Languages.danish, Languages.finnish, Languages.greek, Languages.norwegian, Languages.polish, Languages.russian, Languages.slovenian, Languages.swedish, Languages.turkish, Languages.dutch, Languages.chinese, Languages.japanese, Languages.vietnamese, Languages.indonesian, Languages.persian, Languages.korean, Languages.arabic, Languages.thai, Languages.hindi, Languages.bengali, Languages.tamil, Languages.hungarian, Languages.ukrainian, Languages.slovak, Languages.bulgarian, Languages.catalan, Languages.croatian, Languages.serbian, Languages.lithuanian, Languages.estonian, Languages.hebrew, Languages.latvian, Languages.serbocroatian, Languages.albanian, Languages.azerbaijani, Languages.icelandic, Languages.macedonian, Languages.georgian, Languages.galician, Languages.armenian, Languages.basque ]
 TOP100_LANGUAGES = [ Languages.swahili, Languages.malay, Languages.tagalog, Languages.javanese, Languages.punjabi, Languages.bihari,Languages.gujarati, Languages.yoruba, Languages.marathi, Languages.urdu, Languages.amharic, Languages.telugu, Languages.malayalam, Languages.kannada, Languages.nepali, Languages.kazakh, Languages.belarusian, Languages.burmese, Languages.esperanto, Languages.uzbek, Languages.khmer, Languages.tajik, Languages.welsh, Languages.norwegian_nynorsk, Languages.bosnian, Languages.sinhala, Languages.tatar, Languages.afrikaans, Languages.oriya, Languages.kirghiz, Languages.irish, Languages.occitan, Languages.kurdish, Languages.lao, Languages.luxembourgish, Languages.bashkir, Languages.western_frisian, Languages.pashto, Languages.maltese, Languages.breton, Languages.assamese, Languages.malagasy, Languages.divehi, Languages.yiddish, Languages.somali, Languages.sanskrit, Languages.sindhi, Languages.turkmen, Languages.south_azerbaijani, Languages.sorani, Languages.cebuano, Languages.war ]
-RUN_NAME = "top50_stopwords_p_thresh_0_008_cleaned"
+RUN_NAME = "top50_stopwords_p_thresh_0_008_cleaned_v2"
 
 scratch = os.getenv('SCRATCH')
 
@@ -35,9 +35,7 @@ with open(STATS_FILE, "r") as f:
 
 def is_clean(word):
     word = word.strip() 
-    return "\n" not in word and \
-            word != "" and \
-            word != '–' and \
+    return  word != '–' and \
             word != '—' and \
             word != '’' and \
             word != '’’' and \
@@ -77,6 +75,8 @@ max_avg_word_lengths = {k: v["max_avg_word_length"] for k, v in language_stats.i
 stop_words = { k: to_clean_stopwords(k, v) for k, v in language_stats.items() }
 alpha_ratio = { k: to_alpha_ratio(k, v) for k, v in language_stats.items() }
 min_stop_words = { k: 2 for k, _ in language_stats.items() }
+
+breakpoint()
 
 for lang in TOP50_LANGUAGES + TOP100_LANGUAGES:
     additional_data_path = f'data/datatrove/multi_lingual_{DOC_LIMIT}_{RUN_NAME}/base_processing/{lang}'
